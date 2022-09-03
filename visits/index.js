205231792e8fcf136287f3eda39054698b32736d
Keshav Mundhra <mundhrakeshav11@gmail.com>
2022-09-03 19:19:37 +0530

    Learn Docker Compose

--- visits/index.js
+++ visits/index.js
@@ -2,16 +2,34 @@ const express = require("express");
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
-client.set("visits", 0);
+const client = redis.createClient({
+    // url: 'redis://redis-server:6379'
+    socket: {
+        host: "redis-server",
+        port: 6379
+    }
+});
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.connect().then(async () => {
+    const visits = await client.get("visits");
+    console.log(visits);
+    if (!visits) {
+        client.set("visits", 0);
+    }
+    client.set("visits", visits);
+});
 
-app.get("/", (req, res) => { 
-    client.get("visits", (err, visits) => {
+app.get("/", async (req, res) => { 
+    try {
+        process.exit(1);
+        const visits = await client.get("visits");
         res.send("No of visits is" + visits);
         client.set("visits", parseInt(visits) + 1);
-    })
+    } catch (error) {
+        console.log("ERROR: ", error);
+    }
 })
-
-app.listen(8080, () => {
-    console.log("Listening on 8080");
+    
+app.listen(8081, async () => {
+    console.log("Listening on 8081");
 })
